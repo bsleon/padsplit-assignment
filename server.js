83b1b910b21123a35f24ae47ece55904d5b99067
Brandon Leon <bsleon@umich.edu>
2021-04-25 18:29:37 -0400

    serve compiled react files

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.use(express.json());
 // Add route
 app.use("/rooms", dataRouter);
 
+// This middleware informs the express application to serve our compiled React files
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+	app.use(express.static(path.join(__dirname, "client/build")));
+
+	app.get("*", function (req, res) {
+		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+	});
+}
+
 //connect to the mongo db
 mongoose
 	.connect("mongodb://localhost:27017/PadsplitAssignment" || process.env.REACT_APP_MONGODB_URI, {
